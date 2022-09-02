c2c4e2c8053fc24c016e2fff78c372f67edfd21e
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-09-02 16:08:15 +0530

    schema update for fe fields

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,9 +1,11 @@
+from email.policy import default
 from enum import unique
 import os
-from uuid import UUID
+import logging
 import databases
 import sqlalchemy
-import logging
+from sqlalchemy.sql import func
+
 
 DATABASE_URL = os.getenv(
     "PGSQL_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -17,8 +19,16 @@ urls_table = sqlalchemy.Table(
     "urls",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
     sqlalchemy.Column("url", sqlalchemy.String),
     sqlalchemy.Column("short_url", sqlalchemy.String, unique=True),
+    sqlalchemy.Column("isActive", sqlalchemy.Boolean, default=True),
+    sqlalchemy.Column(
+        "created_at", sqlalchemy.DateTime(timezone=True), server_default=func.now()
+    ),
+    sqlalchemy.Column(
+        "updated_at", sqlalchemy.DateTime(timezone=True), onupdate=func.now()
+    ),
 )
 
 
