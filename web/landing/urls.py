354f965678f627f3274c9fa1bbc9781cafcf8ee4
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-11-05 16:30:57 +0530

    google Oauth2 init

---
+++ web/landing/urls.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022 Nikhil Akki
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", view=views.landing_page, name="Landing_Page"),
+]
