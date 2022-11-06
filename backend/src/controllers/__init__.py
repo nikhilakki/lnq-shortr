1dd51241248b56a11e1515f05b5fb28513edee3c
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-11-07 02:28:45 +0530

    removed (#8)

--- backend/src/controllers/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022 Nikhil Akki
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-from .url import router as url_router
-from .user import router as user_router
-from .quota import router as quota_router
-from .aggregation import router as aggregation_router
--- backend/src/utils/auth.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2022 Nikhil Akki
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-from src import settings
