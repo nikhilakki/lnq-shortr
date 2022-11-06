3086bd234505dda05ed04d0a46e202acb8ad4ef1
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-11-07 00:08:04 +0530

    dashboard and landing pages separated

--- web/landing/models.py
+++ web/landing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class URL(models.Model):
+    long_url = models.TextField()
+    short_url = models.CharField(max_length=12)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
